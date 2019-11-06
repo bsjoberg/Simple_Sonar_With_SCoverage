b50e12147a03c9c4d8082cc6a62e4f0ffa0939cb
bsjoberg <brian.sjoberg@excella.com>
2019-11-06 13:51:57 -0500

    Added fizzbuzz coding kata example

---
+++ src/main/scala/fun/bdd/FizzBuzz.scala
@@ -0,0 +1,24 @@
+package fun.bdd
+
+object FizzBuzz {
+  def countTo(count: Int): String = {
+    var countString: String = ""
+    for (i <- 1 to count) {
+      countString += getResult(i)
+      if (i != count)
+        countString += " "
+    }
+    countString
+  }
+
+  def getResult(i: Int): Any = {
+    if (i % 15 == 0)
+      "fizzbuzz"
+    else if (i % 3 == 0)
+      "fizz"
+    else if (i % 5 == 0)
+      "buzz"
+    else
+      i
+  }
+}
---
+++ src/test/scala/samples/FizzBuzzTest.scala
@@ -0,0 +1,38 @@
+package samples
+
+import fun.bdd.FizzBuzz
+import org.junit.{Assert, Test}
+
+@Test
+class FizzBuzzTest {
+
+  @Test
+  def fizzWhenDivisableBy3(): Unit = {
+    Assert.assertEquals("fizz", FizzBuzz.getResult(3))
+    Assert.assertEquals("fizz", FizzBuzz.getResult(6))
+  }
+
+  @Test
+  def buzzWhenDivisableBy5(): Unit = {
+    Assert.assertEquals("buzz", FizzBuzz.getResult(5))
+    Assert.assertEquals("buzz", FizzBuzz.getResult(10))
+  }
+
+  @Test
+  def fizzBuzzWhenDivisableBy15(): Unit = {
+    Assert.assertEquals("fizzbuzz", FizzBuzz.getResult(15))
+    Assert.assertEquals("fizzbuzz", FizzBuzz.getResult(30))
+  }
+
+  @Test
+  def returnNumberWhenNotDivisibleBy3_5_15(): Unit = {
+    Assert.assertEquals(2, FizzBuzz.getResult(2))
+  }
+
+  @Test
+  def fizzBuzzUpTo20(): Unit = {
+    Assert.assertEquals("1 2 fizz 4 buzz fizz 7 8 fizz buzz 11 fizz 13 14 fizzbuzz 16 17 fizz 19 buzz",
+      FizzBuzz.countTo(20))
+  }
+
+}
